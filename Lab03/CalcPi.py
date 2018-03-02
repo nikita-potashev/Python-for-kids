f6ff3554e8eb19677507600637d1c9281d14e88e
Dzuba <chepik29rus@gmail.com>
2018-03-02 14:26:50 +0300

    changes

--- Lab03/CalcPi.py
+++ Lab03/CalcPi.py
@@ -1,24 +1,22 @@
 def calcPi(n):
   return ( (4*n*n) / ( (2*n-1) * (2*n+1) ) )
 
-epsilon=1e-3
+epsilon=1e-10
 n=1
 pi1=1
 pi2=1
+pi=1
 while True:
-  pi1*=calcPi(n)
-
+  pi1=calcPi(n)
+  pi2=calcPi(n+1)
+  pi*=calcPi(n)
   n+=1
-  pi2*=calcPi(n)
-  print(pi1)
-  
-  if (abs(pi2-pi1)<epsilon):
+  if (abs(pi2-pi1)<=epsilon):
     break
-    print (pi*2)
+  
     
     
-    
-
+print (pi*2)
 
 
 
