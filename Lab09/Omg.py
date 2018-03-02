7820c300956f688428fcfd6ff21672dde724641e
Dzuba <chepik29rus@gmail.com>
2018-03-03 00:27:56 +0300

    Added WORST! task

---
+++ Lab09/Omg.py
@@ -0,0 +1,59 @@
+#One of the most stupid tasks that I ever solved
+
+
+# Sunday = datetime.date(2018,3, 4)
+# Monday = datetime.date(2018,3, 5)  #year, month, day
+# Tuesday = datetime.date(2018,3, 6)
+# print(Sunday.strftime("%A"))
+# print(Monday.strftime("%A"))
+# print(Tuesday.strftime("%A"))
+# print(Sunday==1 and Monday==2 and Tuesday==3)
+
+
+
+from tkinter import *
+import datetime
+
+def out(value):
+    output.delete("0.0","end")
+    output.insert("0.0",str(value)) 
+
+
+
+def calc():
+	Sunday = datetime.date(2018,3, 4)
+	Monday = datetime.date(2018,3, 5)  #year, month, day
+	Tuesday = datetime.date(2018,3, 6)
+	out(Sunday==1 and Monday==2 and Tuesday==3)
+	
+
+
+	
+	
+
+root = Tk()
+root.title("Worst task")
+root.minsize(325,230)
+root.resizable(width=False, height=False)
+
+
+frame = Frame(root)
+frame.grid()
+
+
+a_lab = Label(frame, text="vbSunday==1").grid(row=1,column=2)
+
+
+b_lab = Label(frame, text="vbMonday==2").grid(row=1,column=4)
+
+c_lab = Label(frame, text="vbTuesday==3").grid(row=1,column=6)
+
+
+
+but = Button(frame, text="Calculate", command=calc).grid(row=1, column=7, padx=(10,0))
+
+output = Text(frame, font="Arial 12", width=35, height=10)
+output.grid(row=3, columnspan=8)
+
+
+root.mainloop()
